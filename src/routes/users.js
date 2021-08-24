145017811cd134ed6c8dbce1ea5a18e9db450f4c
Omar Alazaizeh <omx302@gmail.com>
2021-08-24 07:18:00 +0300

    tests

--- src/routes/users.js
+++ src/routes/users.js
@@ -6,15 +6,21 @@ const router = express.Router();
 const basicAuth = require("../middleware/basic-auth");
 const bearerAuth = require("../middleware/bearer-auth");
 const { Users } = require("../models/index");
-// #0000000000000000000000000000000000
+const signUpMiddleware = require("../middleware/signUp");
 
 // {"username":"test", "password":"test"}
-router.post("/signup", (req, res) => {
+router.post("/signup", signUpMiddleware(Users), (req, res) => {
   // check if user name exists
-  console.log(req.body);
-  Users.create(req.body)
-    .then((user) => res.status(201).send(user))
-    .catch((err) => res.status(400).send(err));
+  //   console.log(req.body);
+  //   Users.findOne({ where: { username: req.body.username } }).then((user) => {
+  //     if (!user) {
+  //       Users.create(req.body)
+  //         .then((user) => res.status(201).send(user))
+  //         .catch((err) => res.status(400).send(err));
+  //     } else {
+  //       res.status(400).json("User name exists");
+  //     }
+  //   });
 });
 
 router.post("/signin", basicAuth(Users), (req, res) => {
@@ -22,7 +28,7 @@ router.post("/signin", basicAuth(Users), (req, res) => {
   res.status(200).send(req.user);
 });
 
-router.get("/user", bearerAuth(Users), (req, res) => {
+router.get("/secret", bearerAuth(Users), (req, res) => {
   res.status(200).send(req.user);
 });
 
